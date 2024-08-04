b6c68a99d45db3347a789bc6738a7846899471fe
tanjiro <counter23062001@mail.ru>
2024-08-04 19:13:50 +0300

    add main

---
+++ main.py
@@ -0,0 +1,10 @@
+from user_interaction import (user_interaction,
+                              print_get_all_vacancies,
+                              print_get_companies_and_vacancies_count,
+                              print_get_vacancies_with_higher_salary,
+                              print_get_vacancies_with_keyword,
+                              print_get_all_vacancies_with_some_columns)
+
+
+if __name__ == '__main__':
+    user_interaction()
