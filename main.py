e0141479aedecee7cec43fd7869cbf54eb13da03
tanjiro <counter23062001@mail.ru>
2024-08-07 00:53:08 +0300

    add new func for run

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from user_interaction import (user_interaction,
                               print_get_vacancies_with_higher_salary,
                               print_get_vacancies_with_keyword,
                               print_get_all_vacancies_with_some_columns)
+from sql.db_filler import create_and_fill_tables, drop_tables
 
 
 if __name__ == '__main__':
+    create_and_fill_tables()
     user_interaction()
+    drop_tables()
